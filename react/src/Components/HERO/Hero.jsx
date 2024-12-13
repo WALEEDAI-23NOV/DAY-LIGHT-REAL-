e69233e07ff33f561f7e196273466ebb01dffa87
Waleed Razaq <waleedrazaq75@gmail.com>
2024-12-13 14:52:57 +0500

    as

--- react/src/Components/HERO/Hero.jsx
+++ react/src/Components/HERO/Hero.jsx
@@ -3,13 +3,13 @@ import Nav from "../Nav/Nav";
 
 const Hero = () => {
   return (
-    <div className="w-full border bg-cover bg-center border-indigo-600 bg-[url('/images/Hero.png')] h-[250px] sm:h-[300px]  sm:bg-cover  flex items-center  px-6 md:bg-cover md:h-[550px] md:flex md:items-center md:w-full lg:h-[730px] bg-no-repeat  lg:flex lg:items-center ">
+    <div className="w-full border bg-cover bg-center justify-center items-end sm:items-center sm:flex sm:justify-start sm:px-8 md:px-10 border-indigo-600 md:bg-[url('/images/Hero.png')] bg-[url('/images/Mobile.png')]  h-[100vh] sm:h-[100vh]  sm:bg-cover sm:bg-center  flex   px-4 md:bg-cover md:h-[550px]  md:w-full lg:h-[730px] bg-no-repeat  lg:flex lg:items-center ">
       <div className="fixed top-0 left-0 right-0">
         <Nav />
       </div>
-      <div className=" border border-indigo-600 gap-2 lg:gap-8 md:gap-5 sm:gap-3 flex flex-col justify-center relative w-[150px] sm:w-[370px] md:w-[430px] lg:w-[670px] lg:mx-16">
+      <div className=" border  border-indigo-600 gap-2 lg:gap-8 md:gap-0 sm:gap-3 flex flex-col justify-center relative  w-[400px] sm:w-[450px] md:w-[530px] lg:w-[670px] ">
         <img className="" src="/images/Dead.png" alt="" />
-        <p className="text-white text-[8px] lg:w-[580px] lg:text-lg md:text-base md:w-[400px]">
+        <p className="text-white text-[13px] lg:w-[580px] lg:text-lg md:text-base md:w-[500px]">
           Em Dead by Daylight, persiga, corte e sacrifique sua presa como um
           assassino monstruoso ou junte-se a uma equipe de quatro sobreviventes
           astutos trabalhando juntos para escapar.
--- react/src/Components/Nav/Nav.jsx
+++ react/src/Components/Nav/Nav.jsx
@@ -3,15 +3,17 @@ import React from "react";
 const Nav = () => {
   return (
     <>
-    <div className=" w-full bg-[#090D15] h-14 px-6 opacity-70 md:h-20 flex items-center absolute justify-between md:px-10   ">
-      <div className="flex justify-center items-center">
-        <img className="h-8 md:h-10 lg:h-11" src="/images/logo.png" alt="" />
+      <div className=" w-full bg-[#090D15] h-14 px-6 opacity-70 md:h-20  flex items-center absolute md:px-10   ">
+        <div className="flex justify-center items-center">
+          <img className="h-8 md:h-10 lg:h-11" src="/images/logo.png" alt="" />
+        </div>
       </div>
+      <div className=" relative flex justify-end right-6 top-2 md:top-4 lg:top-4">
+      <button className="h-10  px-4 md:h-11 md:px-6 md:py-2 py-2 lg:h-12 lg:py-3 lg:px-8  bg-[#80080a] justify-center items-center text-white text-base font-semibold  font-['Source Sans Pro'] leading-normal ">
+        Play now
+      </button>
       </div>
-      <div className="h-8 relative px-4 md:h-11 md:px-6 md:py-2 py-2 lg:h-12 lg:py-3 lg:px-8  bg-[#80080a] justify-center items-center gap-2.5 inline-flex">
-        <button className="text-white text-base font-semibold  font-['Source Sans Pro'] leading-normal relative">Jogar agora</button>
-      </div>
-   
+      
     </>
   );
 };
