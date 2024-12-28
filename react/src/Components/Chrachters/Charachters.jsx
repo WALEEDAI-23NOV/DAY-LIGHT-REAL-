283f3d2164912535c4a382e36af56381b60034a0
Waleed Razaq <waleedrazaq75@gmail.com>
2024-12-28 15:19:15 +0500

    as

--- react/src/Components/Chrachters/Charachters.jsx
+++ react/src/Components/Chrachters/Charachters.jsx
@@ -2,78 +2,65 @@ import React from "react";
 
 const Charachters = () => {
   return (
-    <div className="w-full h-[770px] sm:h-[900px] md:h-[650px] lg:h-[900px] border ">
-      <div className="w-full flex md:flex-row h-[80%] md:h-[70%] sm:h-[90%]">
-        <div className="border flex flex-col bg-[url('/images/Charachter-bg.png')] bg-center bg-cover bg-no-repeat h-full w-1/2">
-          <div className="md:h-[30%] hidden  border md:flex items-center justify-center">
-            <h1 className="md:text-4xl  text-center text-white">
-              Play as
-              <br />A Killer (Solo)
-            </h1>
-          </div>
-          <div className="flex flex-col   md:flex-row md:h-[70%] h-[100%] border bg-black">
-            <div className="flex   justify-center items-end h-1/2 md:w-1/2 md:h-full bg-[url('/images/Charachter-ghost-bg.png')] bg-center bg-cover border">
-              <img
-                className=" max-w-full h-[100%] object   "
-                src="/images/ghost.png"
-                alt="Ghost"
-              />
-              <img
-                className="absolute w-[150px] md:w-[250px]"
-                src="/images/ghost-text.png"
-                alt="Ghost Text"
-              />
-            </div>
-            <div className="flex   justify-center items-end h-1/2 md:w-1/2 md:h-full bg-[url('/images/Charachter-ghost-bg.png')] bg-center bg-cover border">
-              <img
-                className=" w-full h-full object-cover"
-                src="/images/donkey.png"
-                alt="Ghost"
-              />
-              <img
-                className="absolute w-[150px] md:w-[250px]"
-                src="/images/ghost-text.png"
-                alt="Ghost Text"
-              />
-            </div>
+    <div className="flex flex-col lg:flex-row gap-4 p-6 lg:p-2 bg-[#090d15] text-white">
+    <div className="flex-1 bg-[url('/images/Charachter-bg.png')] bg-cover bg-center rounded-lg p-6 shadow-lg">
+      <h1 className="text-xl md:text-2xl lg:text-3xl  mb-14 text-center lg:text-center">
+      Play as<br />A Killer (Solo)
+      </h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col bg-center bg-cover bg-no-repeat bg-[url('/images/Charachter-ghost-bg.png')] items-center">
+          <div className="flex flex-col relative h-full justify-end md:w-full items-center mt-auto">
+            <img
+              src="images/ghost.png"
+              alt="Ghost Face"
+              className="rounded-lg w-full object-contain"
+            />
+            <img className="absolute w-[150px] sm:w-[200px] md:w-[250px] lg:w-[230px]" src="/images/ghost-text.png" alt="" />
+            
           </div>
         </div>
-        <div className="border bg-[url('/images/Charachter-bg1.png')] bg-center bg-no-repeat bg-cover h-full w-1/2">
-          <div className="md:h-[30%]  sm:h-[10%] border  items-center justify-center hidden md:flex">
-            <h1 className="md:text-4xl text-center text-white">
-              Play as
-              <br />A Survivor (Co-op)
-            </h1>
-          </div>
-          <div className="flex flex-col   md:flex-row md:h-[70%] h-[100%] border bg-black">
-            <div className="flex   justify-center items-end h-1/2 md:w-1/2 md:h-full bg-[url('/images/Charachter-ghost-bg.png')] bg-center bg-cover border">
-              <img
-                className=" max-w-full h-[100%] object   "
-                src="/images/ghost.png"
-                alt="Ghost"
-              />
-              <img
-                className="absolute w-[150px] md:w-[250px]"
-                src="/images/ghost-text.png"
-                alt="Ghost Text"
-              />
-            </div>
-            <div className="flex   justify-center items-end h-1/2 md:w-1/2 md:h-full bg-[url('/images/Charachter-ghost-bg.png')] bg-center bg-cover border">
-              <img
-                className=" w-full h-full object-cover"
-                src="/images/donkey.png"
-                alt="Ghost"
-              />
-              <img
-                className="absolute w-[150px] md:w-[250px]"
-                src="/images/ghost-text.png"
-                alt="Ghost Text"
-              />
-            </div>
+        <div className="flex flex-col bg-center  bg-contain bg-no-repeat bg-[url('/images/Charachter-donkey-bg.png')] items-center">
+          <div className="flex relative flex-col h-full justify-end md:w-full items-center mt-auto">
+            <img
+              src="images/donkey.png"
+              alt="Saw"
+              className="rounded-lg w-full object-contain"
+            />
+             <img className="absolute w-[100px] sm:w-[150px]  md:w-[130px] lg:w-[150px]" src="/images/donkey-text.png" alt="" />
           </div>
         </div>
       </div>
     </div>
+  
+    <div className="flex-1 bg-[url('/images/Charachter-bg1.png')] bg-cover bg-center rounded-lg p-6 shadow-lg">
+      <h1 className="text-xl md:text-2xl lg:text-3xl  mb-14 text-center lg:text-center">
+      Play as<br />A survivor(Co-op)
+      </h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col bg-center bg-cover bg-no-repeat bg-[url('/images/Charachter-bg2.png')] items-center">
+          <div className="flex flex-col relative h-full justify-end md:w-full items-center mt-auto">
+            <img
+              src="images/Residentevil.png"
+              alt="Resident Evil"
+              className="rounded-lg w-full object-contain"
+            />
+            <img className="absolute w-[150px] sm:w-[200px]  md:w-[250px] lg:w-[230px]" src="/images/residentevil-text.png" alt="" />
+          </div>
+        </div>
+        <div className="flex flex-col bg-center bg-cover bg-no-repeat bg-[url('/images/Charachter-bg3.png')] items-center">
+          <div className="flex flex-col relative h-full justify-end items-center md:w-full mt-auto">
+            <img
+              src="images/silenthill.png"
+              alt="Silent Hill"
+              className="rounded-lg w-full object-contain"
+            />
+            <img className="absolute w-[150px] sm:w-[200px]  md:w-[250px] lg:w-[230px] pb-2" src="/images/silenthill-text.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
   );
 };
 
